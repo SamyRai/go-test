9df9b058fe4ce9def3a108f80e5434246db85c27
Damir Mukimov <damir.mukimov@here.com>
2020-06-10 17:21:12 +0200

    Fix CR issues

--- homework-2/main_test.go
+++ homework-2/main_test.go
@@ -9,17 +9,13 @@ func TestIsEvenNumber(t *testing.T) {
 	oddNumbers := [6]int{1, 3, 5, 7, 9, 11}
 
 	for _, evenNumber := range evenNumbers {
-		if isEvenNumber(evenNumber) {
-			t.Logf("%d is an even number", evenNumber)
-		} else {
+		if !isEvenNumber(evenNumber) {
 			t.Errorf("%d is an odd number", evenNumber)
 		}
 	}
 	for _, oddNumber := range oddNumbers {
 		if isEvenNumber(oddNumber) {
 			t.Errorf("%d is an even number", oddNumber)
-		} else {
-			t.Logf("%d is an odd number", oddNumber)
 		}
 	}
 }
@@ -40,9 +36,7 @@ func TestIsDivisible(t *testing.T) {
 	}
 
 	for _, item := range testItems {
-		if isDivisible(item.number, item.divider) == item.result {
-			t.Logf("isDivisible(%d, %d) returns %t", item.number, item.divider, item.result)
-		} else {
+		if isDivisible(item.number, item.divider) != item.result {
 			t.Errorf("isDivisible(%d, %d) returns %t", item.number, item.divider, item.result)
 		}
 	}
@@ -52,9 +46,7 @@ func TestFib(t *testing.T) {
 	fibNumbers := [21]uint{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765}
 
 	for i, fibNumber := range fibNumbers {
-		if fib(uint(i)) == fibNumber {
-			t.Logf("fib(%d) = %d", i, fibNumber)
-		} else {
+		if fib(uint(i)) != fibNumber {
 			t.Errorf("fib(%d) != %d", i, fibNumber)
 		}
 	}
